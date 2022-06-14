6bdd25bdd264d6d7ac187ec5165688cfa83c9292
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-06-13 23:15:47 -0300

    Desafio finalizado

---
+++ script.js
@@ -0,0 +1,31 @@
+//FOR CRESCENTE
+let contagemProgressiva = 0;
+for(contagemProgressiva = 1; contagemProgressiva <= 10; contagemProgressiva++){
+    console.log(contagemProgressiva);
+}
+
+console.log('----------------------')
+
+//WHILE DESCRECENTE
+let contagemRegressiva = 10;
+while(contagemRegressiva >= 1){
+    console.log(contagemRegressiva);
+    contagemRegressiva--;
+}
+
+console.log('----------------------')
+
+//NÚMEROS ÍMPARES
+let numImpar = 1;
+for(numImpar = 1; numImpar <= 100; numImpar += 2){
+    console.log(numImpar);
+}
+
+console.log('----------------------')
+
+//NÚMEROS PARES
+let numPar = 0;
+while(numPar <= 100){
+    console.log(numPar);
+    numPar += 2;
+}
